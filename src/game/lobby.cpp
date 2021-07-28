aecba4f337660065b5b17216955e1f7d3aeee5bd
pyrun <thenightalex@googlemail.com>
2021-07-28 16:19:12 +0200

    flag for enitity changes to reduce network traffic

--- src/game/lobby.cpp
+++ src/game/lobby.cpp
@@ -3,6 +3,8 @@
 using namespace engine;
 using namespace game;
 
+#include "../engine/timer.h"
+
 lobby::lobby() {
     p_font = NULL;
 }
@@ -18,6 +20,7 @@ void lobby::init( engine::font *font, engine::input_map *input, engine::entity_h
 }
 
 void lobby::draw( engine::graphic_draw *graphic) {
+    static uint32_t l_time = 0;
     if( !p_font)
         return;
     if( p_entity->get( 2)) {
@@ -29,8 +32,13 @@ void lobby::draw( engine::graphic_draw *graphic) {
             p_entity->get( 2)->position.y -= 1;
         if( p_input->y > 0)
             p_entity->get( 2)->position.y += 1;
+        p_entity->get( 2)->change = true;
     }
-    
+
+    p_font->print( { 100, 80}, "%d ms", helper::time::getDurrent(&l_time)-16);
+    helper::time::reset( &l_time);
+
+
     p_font->print( { 100, 50}, "test %d %d %d %d amount", p_input->x, p_input->y, graphic->getDisplacedElements() , p_entity->getAmount());
 
     p_font->draw( "{dies ist ein sehr langer test} mit ��� ? und anderen sonderzeichen/()", vec2{ 100, 100});
