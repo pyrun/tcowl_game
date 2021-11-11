2e0a788319f1be49081812a1910099dfd9cff68c
Pyrun <thenightalex@googlemail.com>
2021-11-11 09:22:16 +0100

    testing vosk

--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,45 @@
 
 #include "game/app.h"
 
+#include <vosk_api.h>
+
 // https://donjon.bin.sh/fantasy/name/#type=set;set=deity
 // https://opengameart.org/content/2d-modified-dark-forest-tileset
 // https://opengameart.org/content/forest-tileset-for-rpgs
+void test() {
+    FILE *wavin;
+    char buf[3200];
+    int nread, final;
+
+    VoskModel *model = vosk_model_new("model");
+    VoskRecognizer *recognizer = vosk_recognizer_new(model, 16000.0);
+
+    wavin = fopen("test.wav", "rb");
+    fseek(wavin, 44, SEEK_SET);
+    while (!feof(wavin)) {
+         nread = fread(buf, 1, sizeof(buf), wavin);
+         final = vosk_recognizer_accept_waveform(recognizer, buf, nread);
+         if (final) {
+             printf("%s\n", vosk_recognizer_result(recognizer));
+         } /*else {
+             printf("%s\n", vosk_recognizer_partial_result(recognizer));
+         }*/
+    }
+    printf("%s\n", vosk_recognizer_final_result(recognizer));
+
+    vosk_recognizer_free(recognizer);
+    vosk_model_free(model);
+    fclose(wavin);
+    exit(0);
+    return;
+}
 
 int main( int argc, char* args[] ) {
     sdl::init();
     game::app l_app;
     bool l_start_server = false;
+
+    test();
 
     // check args
     for( uint32_t i = 0; i < argc; i++) {
