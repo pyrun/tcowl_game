8d3f441f4fcf7b7061e37ac6caba12c0c786cba7
Pyrun <thenightalex@googlemail.com>
2021-11-17 17:26:51 +0100

    Physic: Handling collisions with multiple objects differently

--- src/engine/physic/hub.cpp
+++ src/engine/physic/hub.cpp
@@ -29,7 +29,7 @@ bool hub::testAABBAABB( const engine::fvec2 &rectPos1, const engine::fvec2 &rect
 float hub::sweptAABB(   const engine::fvec2 &pos1, const engine::fvec2 &rect1,
                         const engine::fvec2 &pos2, const engine::fvec2 &rect2,
                         engine::fvec2 vel,
-                        engine::fvec2 *normal) {
+                        engine::vec2 *normal) {
     engine::fvec2 l_invEntry;
     engine::fvec2 l_invExit;
 
@@ -75,26 +75,26 @@ float hub::sweptAABB(   const engine::fvec2 &pos1, const engine::fvec2 &rect1,
 
     // if there was no collision
     if ( l_entryTime > l_exitTime || l_entry.x < 0.0f && l_entry.y < 0.0f || l_entry.x > 1.0f || l_entry.y > 1.0f) {
-        normal->x = 0.0f;
-        normal->y = 0.0f;
+        normal->x = 0;
+        normal->y = 0;
         return 1.0f; 
     } else {// if there was a collision 
         // calculate normal of collided surface
         if( l_entry.x > l_entry.y) {
             if ( l_invEntry.x < 0.0f) {
-                normal->x = 1.0f; 
-                normal->y = 0.0f; 
+                normal->x = 1; 
+                normal->y = 0; 
             } else {
-                normal->x = -1.0f; 
-                normal->y = 0.0f;
+                normal->x = -1; 
+                normal->y = 0;
             }
         } else {
             if ( l_invEntry.y < 0.0f) {
-                normal->x = 0.0f; 
-                normal->y = 1.0f; 
+                normal->x = 0; 
+                normal->y = 1; 
             } else {
-                normal->x = 0.0f; 
-                normal->y = -1.0f; 
+                normal->x = 0; 
+                normal->y = -1; 
             }
         }
     }
@@ -103,9 +103,11 @@ float hub::sweptAABB(   const engine::fvec2 &pos1, const engine::fvec2 &rect1,
 
 void hub::update( float dt) {
     float l_friction = 0.25f; //todo woher kommt es? was beeinflusst es
-    engine::fvec2 l_normal;
+    engine::vec2 l_normal;
 
     for( uint32_t i = 0; i < p_bodys.size(); i++) {
+        float l_collisiontime = 1.0f;
+        engine::vec2 l_face = { 0, 0};
         body* l_body = p_bodys[i];
 
         l_body->setCollied( false);
@@ -117,21 +119,29 @@ void hub::update( float dt) {
                                     p_bodys[n]->getPosition() + p_bodys[n]->getShape()->getOffset(), p_bodys[n]->getShape()->getAABB()) &&
                                     i != n) {// nicht gegen sich selbst
                     l_body->setCollied( true);
-                    float collisiontime = sweptAABB(    l_body->getPosition() + l_body->getShape()->getOffset(), l_body->getShape()->getAABB(),
+                    float l_collisiontime2 = sweptAABB(    l_body->getPosition() + l_body->getShape()->getOffset(), l_body->getShape()->getAABB(),
                                                         p_bodys[n]->getPosition() + p_bodys[n]->getShape()->getOffset(), p_bodys[n]->getShape()->getAABB(),
                                                         (l_body->getVelocity() * dt),
                                                         &l_normal);
                     
-                    // Auf normal reagieren
-                    l_body->addPosition( (l_body->getVelocity() * dt) * collisiontime);
-                    l_body->setVelocity( {  l_normal.x?0.f:l_body->getVelocity().x,
-                                            l_normal.y?0.f:l_body->getVelocity().y} );
+                    // Prüfen ob relavant
+                    if( l_collisiontime2 < l_collisiontime) {
+                            l_collisiontime  = l_collisiontime2;
+                        
+                        // wert behandeln
+                        l_face.x = l_normal.x?l_normal.x:l_face.x;
+                        l_face.y = l_normal.y?l_normal.y:l_face.y;
+                    }
                 }
             }
         }
 
         // velocity
-        l_body->addPosition( l_body->getVelocity() * dt);
+        l_body->addPosition( l_body->getVelocity() * dt * l_collisiontime);
+
+        // Auf face reagieren
+        l_body->setVelocity( {  l_face.x?0.f:l_body->getVelocity().x,
+                                l_face.y?0.f:l_body->getVelocity().y} );
 
         // friction
         l_body->setVelocity( {  helper::lerp( l_body->getVelocity().x, 0, l_friction),
