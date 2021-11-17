e0ee81a9a8d86cb739ec38c4420e1e29e04b7f8d
pyrun <thenightalex@googlemail.com>
2021-11-18 00:05:43 +0100

    lizzard freddy hinzugefügt

---
+++ res/entity/lizzard/script.lua
@@ -0,0 +1,39 @@
+local velocity_window = 0.1
+local walk_speed = 10.0
+
+function update( id)
+	local l_velX, l_velY
+
+	if entity.isAlive( id) == false then
+		do return end
+	end
+
+	if entity.isInputPresent( id) then
+		local l_inputX, l_inputY
+		l_inputX, l_inputY = entity.getInputAxies( id)
+
+		entity.doVelocity( id, walk_speed*l_inputX, walk_speed*l_inputY)
+	end
+
+	l_velX, l_velY = entity.getVelocity( id)
+
+	if math.abs(l_velX) > math.abs(l_velY) then
+		if l_velX < -velocity_window then
+			entity.setAnimation( id, "Walk4")
+			-- Links
+		end
+		if l_velX > velocity_window then
+			entity.setAnimation( id, "Walk2")
+			-- Rechts
+		end
+	else
+		if l_velY < -velocity_window then
+			entity.setAnimation( id, "Walk3")
+			-- Runter
+		end
+		if l_velY > velocity_window then
+			entity.setAnimation( id, "Walk1")
+			-- Hoch
+		end
+	end
+end
