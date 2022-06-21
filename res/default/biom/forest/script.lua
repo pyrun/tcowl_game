55e53d40af9cccff8ec9502b85ca6057b5ede957
Pyrun <thenightalex@googlemail.com>
2022-06-21 14:13:48 +0200

    cleanup lua

--- res/default/biom/forest/script.lua
+++ res/default/biom/forest/script.lua
@@ -1,12 +1,12 @@
-rooms = {}
+local rooms = {}
 
-function addRoom( x, y, w, h)
+function AddRoom( x, y, w, h)
     table.insert( rooms, { x = x, y = y, w = w, h = h})
 end
 
-function checkSpaceRoom( x, y, w, h, space)
+function CheckSpaceRoom( x, y, w, h, space)
     -- Check world border
-    world_size_x, world_size_y = world.getSize()
+    local world_size_x, world_size_y = tcowl.world.getSize()
     if space > x or space > y or world_size_x - space < x+w or world_size_y - space < y+h then
         return false
     end
@@ -24,34 +24,34 @@ function checkSpaceRoom( x, y, w, h, space)
     return true
 end
 
-function intilisation( try)
+function Intilisation( try)
     print("Biom grass script")
 
-    world_size_x, world_size_y = world.getSize()
+    local world_size_x, world_size_y = tcowl.world.getSize()
 
-    room_min_length = 2
-    room_max_length = 10
+    local room_min_length = 2
+    local room_max_length = 10
 
     for i = 1, 5000 do
-        room = {}
+        local room = {}
         room.x = math.random(0,world_size_x)
         room.y = math.random(0,world_size_y)
         room.w = math.random( room_min_length, room_max_length)
         room.h = math.random( room_min_length, room_max_length)
 
-        result = checkSpaceRoom( room.x, room.y, room.w, room.h, 1)
+        local result = CheckSpaceRoom( room.x, room.y, room.w, room.h, 1)
         if result then
-            addRoom( room.x, room.y, room.w, room.h)
+            AddRoom( room.x, room.y, room.w, room.h)
         end
     end
 end
 
-function set( x, y, value)
-    world.setTile( 2, x, y)
+function Set( x, y, value)
+    tcowl.world.setTile( 2, x, y)
 
     for i, room in pairs(rooms) do
         if (room.x <= x and room.y <= y and room.x+room.w > x and room.y+room.h > y) then
-            world.setTile( 1, x, y)
+            tcowl.world.setTile( 1, x, y)
         end
     end
 end
